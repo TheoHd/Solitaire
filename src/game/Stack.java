253de31d5c70beb30a4faacef64a9f01ca1c08ad
Baptiste Vasseur <bvasseur77@gmail.com>
2017-11-09 22:37:17 +0100

    update

--- src/game/Stack.java
+++ src/game/Stack.java
@@ -26,34 +26,26 @@ public class Stack {
 
 			Card visile_card = this.cartes.get(i);
 			visile_card.setVisible(1);
-			this.row1.add(visile_card);
+			if(i == 0) { this.row1.add(visile_card); }
+			if(i == 1) { this.row2.add(visile_card); }
+			if(i == 2) { this.row3.add(visile_card); }
+			if(i == 3) { this.row4.add(visile_card); }
+			if(i == 4) { this.row5.add(visile_card); }
+			if(i == 5) { this.row6.add(visile_card); }
+			if(i == 6) { this.row7.add(visile_card); }
 			this.cartes.remove(i);
 
 			for (int j = 0; j <= i-1; j++){
 
 				Card hidden_card = this.cartes.get(j);
 				hidden_card.setVisible(0);
-				if(i == 0) {
-					this.row1.add(hidden_card);
-				}
-				if(i == 1) {
-					this.row2.add(hidden_card);
-				}
-				if(i == 2) {
-					this.row3.add(hidden_card);
-				}
-				if(i == 3) {
-					this.row4.add(hidden_card);
-				}
-				if(i == 4) {
-					this.row5.add(hidden_card);
-				}
-				if(i == 5) {
-					this.row6.add(hidden_card);
-				}
-				if(i == 6) {
-					this.row7.add(hidden_card);
-				}
+				if(i == 0) { this.row1.add(hidden_card); }
+				if(i == 1) { this.row2.add(hidden_card); }
+				if(i == 2) { this.row3.add(hidden_card); }
+				if(i == 3) { this.row4.add(hidden_card); }
+				if(i == 4) { this.row5.add(hidden_card); }
+				if(i == 5) { this.row6.add(hidden_card); }
+				if(i == 6) { this.row7.add(hidden_card); }
 				this.cartes.remove(j);
 			}
 		}
